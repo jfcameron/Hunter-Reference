96998f67d53af024b34b12ffb8645eeacb891e7c
Joseph Cameron <jf.cameron@gmail.com>
2018-04-04 18:58:30 -0400

    wip

--- include/Hunter-Reference/object.h
+++ include/Hunter-Reference/object.h
@@ -7,13 +7,18 @@
 
 namespace hunterreference
 {
+    /*!
+     An object with a secret and a graphics test.
+     */
     class object
     {
         int secret = 123;
 
     public:
+        /// gives access to the secret
         int getSecret() const;
 
+        /// runs a neat opengl thing
         void runGraphicsTest();
     };
 }
