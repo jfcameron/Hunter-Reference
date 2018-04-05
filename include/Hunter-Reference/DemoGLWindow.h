5dd1c3818d8f00ebcbf94cc83ec54b7623e76133
Joseph Cameron <jf.cameron@gmail.com>
2018-04-05 13:02:24 -0400

    wip

--- include/Hunter-Reference/DemoGLWindow.h
+++ include/Hunter-Reference/DemoGLWindow.h
@@ -5,6 +5,8 @@
 #ifndef HUNTER_REFERENCE_H
 #define HUNTER_REFERENCE_H
 
+#include <memory>
+
 struct GLFWwindow;
 
 namespace gfx
@@ -12,9 +14,9 @@ namespace gfx
     /*!
      Crossplatform window that plays a OpenGL < 2.0 demo
      */
-    class DemoGLWindow
+    class DemoGLWindow final
     {
-        GLFWwindow *const pWindow;
+        std::unique_ptr<GLFWwindow, void(*)(GLFWwindow *const)> m_pWindow;
 
     public:
         /// Indicates whether the window should close or not
